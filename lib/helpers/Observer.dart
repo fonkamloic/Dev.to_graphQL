32b1f92bec49b3802372f1cf09cf79aece804d3d
fonkamloic <fonkamloic@gmail.com>
2020-02-24 03:19:07 +0100

    observer

---
+++ lib/helpers/Observer.dart
@@ -0,0 +1,40 @@
+
+
+import 'package:flutter/material.dart';
+
+class Observer<T> extends StatelessWidget {
+  @required
+  final Stream<T> stream;
+
+  @required
+  final Function onSuccess;
+
+  final Function onError;
+  final Function onWaiting;
+
+  const Observer({this.onSuccess, this.onWaiting, this.stream, this.onError});
+
+  Function get _defaultOnWaiting => (context) => Center(child: CircularProgressIndicator());
+
+  Function get _defaultOnError => (context, error ) => Text(error);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: stream,
+      builder: (context, AsyncSnapshot<T> snapshot){
+        if (snapshot.hasError){
+          return (onError != null)? onError(context, snapshot.error) : _defaultOnError(context, snapshot.error);
+        }
+        if (snapshot.hasData){
+          T data = snapshot.data;
+          return onSuccess(context , data);
+
+        }else {
+          return (onWaiting != null) ? onWaiting(context) : _defaultOnWaiting(context);
+        }
+      },
+    );
+  }
+}
